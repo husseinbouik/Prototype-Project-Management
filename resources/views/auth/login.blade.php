9cd2dfa01547fbfeecdefbe8fa32eac2ca2e7197
husseinbouik <husseinbouik5@gmail.com>
2023-12-21 16:48:51 +0100

    Add admin-lte package and update routes and dependencies

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,83 +1,71 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Log in (v2)</title>
+<x-laravel-ui-adminlte::adminlte-layout>
 
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
-  <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="css/adminlte.min.css">
-</head>
-<body class="hold-transition login-page">
-<div class="login-box">
-  <!-- /.login-logo -->
-  <div class="card card-outline card-primary">
-    <div class="card-header text-center">
-      <a href="index2.html" class="h1"><b>Projects </b>Managment</a>
-    </div>
-    <div class="card-body">
-      <p class="login-box-msg">Sign in to start your journey</p>
+    <body class="hold-transition login-page">
+        <div class="login-box">
+            <div class="login-logo">
+                <a href="{{ url('/home') }}"><b>{{ config('app.name') }}</b></a>
+            </div>
+            <!-- /.login-logo -->
 
-      <form method="POST"
-      action="{{route('Auth') }}">
-      @csrf
-        <div class="input-group mb-3">
-          <input type="email" class="form-control" name="email" placeholder="Email">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
-            </div>
-          </div>
-          @error('email')
-          <div class="text-danger">{{ $message }}</div>
-          @enderror
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" name="password" placeholder="Password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-          @error('password')
-          <div class="text-danger">{{ $message }}</div>
-          @enderror
-        </div>
-        <div class="row">
-          <div class="col-8">
-            <div class="icheck-primary">
-              <input type="checkbox" id="remember">
-              <label for="remember">
-                Remember Me
-              </label>
-            </div>
-          </div>
-          <!-- /.col -->
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
-      <!-- /.social-auth-links -->
-    </div>
-    <!-- /.card-body -->
-  </div>
-  <!-- /.card -->
-</div>
-<!-- /.login-box -->
+            <!-- /.login-box-body -->
+            <div class="card">
+                <div class="card-body login-card-body">
+                    <p class="login-box-msg">Sign in to start your session</p>
 
-<!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="js/adminlte.min.js"></script>
-</body>
-</html>
+                    <form method="post" action="{{ url('/login') }}">
+                        @csrf
+
+                        <div class="input-group mb-3">
+                            <input type="email" name="email" value="{{ old('email') }}" placeholder="Email"
+                                class="form-control @error('email') is-invalid @enderror">
+                            <div class="input-group-append">
+                                <div class="input-group-text"><span class="fas fa-envelope"></span></div>
+                            </div>
+                            @error('email')
+                                <span class="error invalid-feedback">{{ $message }}</span>
+                            @enderror
+                        </div>
+
+                        <div class="input-group mb-3">
+                            <input type="password" name="password" placeholder="Password"
+                                class="form-control @error('password') is-invalid @enderror">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-lock"></span>
+                                </div>
+                            </div>
+                            @error('password')
+                                <span class="error invalid-feedback">{{ $message }}</span>
+                            @enderror
+
+                        </div>
+
+                        <div class="row">
+                            <div class="col-8">
+                                <div class="icheck-primary">
+                                    <input type="checkbox" id="remember">
+                                    <label for="remember">Remember Me</label>
+                                </div>
+                            </div>
+
+                            <div class="col-4">
+                                <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                            </div>
+
+                        </div>
+                    </form>
+
+                    <p class="mb-1">
+                        <a href="{{ route('password.request') }}">I forgot my password</a>
+                    </p>
+                    <p class="mb-0">
+                        <a href="{{ route('register') }}" class="text-center">Register a new membership</a>
+                    </p>
+                </div>
+                <!-- /.login-card-body -->
+            </div>
+
+        </div>
+        <!-- /.login-box -->
+    </body>
+</x-laravel-ui-adminlte::adminlte-layout>
--- routes/web.php
+++ routes/web.php
@@ -60,3 +60,11 @@ Route::prefix('members')->group(function () {
 
 
 
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
